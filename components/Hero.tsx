c3205ee7b1fe10407fc7ef86e4e47d0a4b5846bb
JERICHO URBANO <jerichourbano.01.01.04@gmail.com>
2026-08-16 22:00:44 +0800

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -107,11 +107,12 @@ export default function Hero() {
         style={{ maxWidth: '650px' }}
       >
         <Image
-          src="/img/hero.png"
+          src="/img/hero.webp"
           alt="Jericho Urbano portrait"
           fill
           priority
-          sizes="(max-width: 650px) 100vw, 650px"
+          quality={90}
+          sizes="(max-width: 650px) 100vw, (max-width: 1200px) 75vw, 650px"
           className="object-cover object-bottom"
         />
       </div>
