facf9e2819e6eb401d5561603870bac03fd642d5
JERICHO URBANO <jerichourbano.01.01.04@gmail.com>
2026-08-16 23:13:00 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    qualities: [75, 90],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
