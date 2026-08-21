04d8803ca4ebbd5a4b3dd4295f8352329eca45d4
JERICHO URBANO <jerichourbano.01.01.04@gmail.com>
2026-08-21 17:31:44 +0800

    Update metadata.ts

--- app/metadata.ts
+++ app/metadata.ts
@@ -7,7 +7,21 @@ export const viewportConfig: Viewport = {
   colorScheme: "dark",
 };
 
+const getBaseUrl = () => {
+  if (process.env.NEXT_PUBLIC_SITE_URL) {
+    return process.env.NEXT_PUBLIC_SITE_URL;
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:3000";
+  }
+  return "https://portfolio-jerichourbano.vercel.app";
+};
+
 export const siteMetadata: Metadata = {
+  metadataBase: new URL(getBaseUrl()),
   title: {
     default: "Jericho Urbano | Portfolio",
     template: "%s | Jericho Urbano",
